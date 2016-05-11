95fca678a5c89dd35ad773a0f97927278c954e6e
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-05-11 15:16:54 +0200

    WIP controller

---
+++ src/Rubedographql/Frontoffice/Controller/RgqlController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rubedographql\Frontoffice\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\JsonModel;
+
+class RgqlController extends AbstractActionController
+{
+    public function indexAction(){
+        die("test");
+    }
+}
