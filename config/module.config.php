b0b54216e5979b7278e8af4fabf3063dcc9dd1df
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-05-11 15:34:41 +0200

    Revert "WIP controller"
    This reverts commit 95fca678a5c89dd35ad773a0f97927278c954e6e.

--- config/module.config.php
+++ config/module.config.php
@@ -8,39 +8,4 @@ return array(
     'namespaces_api' => array(
         'Rubedographql',
     ),
-    'controllers' => array(
-        'invokables' => array(
-            'Rubedographql\\Frontoffice\\Controller\\Rgql' => 'Rubedographql\\Frontoffice\\Controller\\RgqlController',
-        ),
-    ),
-    'router' => array (
-        'routes' => array(
-            'rubedo-graphql' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route' => '/graphql',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Rubedographql\\Frontoffice\\Controller',
-                        'controller' => 'stripe-payment-logs',
-                        'action' => 'index'
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/[:action]',
-                            '__NAMESPACE__' => 'Rubedographql\\Frontoffice\\Controller',
-                            'constraints' => array(
-                                'controller' => 'rgql',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*'
-                            ),
-                            'defaults' => array()
-                        )
-                    )
-                )
-            ),
-        ),
-    ),
 );
