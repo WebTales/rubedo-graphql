a85dcc45898e518394d26ac472ef5017680f656d
Didier Fanchon <didier.fanchon@webtales.fr>
2016-05-12 15:03:18 +0200

    Add simple search wrapper

--- src/Rubedographql/Connector/RGQLRubedoConector.php
+++ src/Rubedographql/Connector/RGQLRubedoConector.php
@@ -10,32 +10,40 @@ use \Exception;
 class RGQLRubedoConnector
 {
     public function resolve($configs=[],$args=[],$multivalued=false){
-        if(empty($configs["collection"])){
-            throw new Exception('RGQLRubedoConnector requires valid collection');
+        if(empty($configs["collection"])&&empty($configs["index"])){
+            throw new Exception('RGQLRubedoConnector requires a valid collection or index');
         }
-        $filter=Filter::factory();
-        foreach($args as $key=>$value){
-            if($key=="id"){
-                if (is_array($value)){
-                    $filter->addFilter(Filter::factory("InUid")->setValue($value));
-                } else {
-                    $filter->addFilter(Filter::factory("Uid")->setValue($value));
-                }
-            } else {
-                if (is_array($value)){
-                    $filter->addFilter(Filter::factory("In")->setName($key)->setValue($value));
-                } else {
-                    $filter->addFilter(Filter::factory("Value")->setName($key)->setValue($value));
-                }
-            }
+        if (!(empty($configs["collection"]))) {
+          $filter=Filter::factory();
+          foreach($args as $key=>$value){
+              if($key=="id"){
+                  if (is_array($value)){
+                      $filter->addFilter(Filter::factory("InUid")->setValue($value));
+                  } else {
+                      $filter->addFilter(Filter::factory("Uid")->setValue($value));
+                  }
+              } else {
+                  if (is_array($value)){
+                      $filter->addFilter(Filter::factory("In")->setName($key)->setValue($value));
+                  } else {
+                      $filter->addFilter(Filter::factory("Value")->setName($key)->setValue($value));
+                  }
+              }
+          }
+          $service=Manager::getService($configs["collection"]);
+          if($multivalued){
+              $result=$service->getList($filter)["data"];
+          } else {
+              $result=$service->findOne($filter);
+          }
         }
-        $service=Manager::getService($configs["collection"]);
-        if($multivalued){
-            $result=$service->getList($filter)["data"];
-        } else {
-            $result=$service->findOne($filter);
+        if (!(empty($configs["index"]))) {
+          $query = Manager::getService("ElasticDataSearch");
+          $query->init();
+          $result=$query->search($args,"all");
+          $result = $result["data"];
         }
-        return $result;
+      return $result;
     }
 
 }
